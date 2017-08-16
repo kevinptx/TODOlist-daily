bca8e68e60d8760f200c78720e524ffd8623c224
kevinptx <kevin.peery@gmail.com>
2017-08-16 17:45:40 -0500

    almost done

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const app = express()
+const mustache = require("mustache-express")
+const bodyParser = require("body-parser")
+var expressValidator = require('express-validator');
+app.engine("mustache", mustache())
+app.set("view engine", "mustache")
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(expressValidator());
+
+var todos = []
+var completed = []
+
+app.get("/", function (req, res) {
+  res.render('index', {
+    todos: todos,
+    completed: completed
+  });
+});
+
+app.post("/", function (req, res) {
+  todos.push(req.body.todo);
+  res.redirect('/');
+})
+
+app.post('/completed', function (req, res) {
+  const remove = req.body.completed
+  todos.splice(todos.indexOf(remove), 1)
+  completed.push(remove);
+  res.redirect('/');
+})
+
+
+app.listen(3000, function() {
+  console.log("Listening on 3000")
+})
